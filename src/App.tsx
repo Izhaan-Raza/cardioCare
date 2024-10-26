d36a69ab22a383fd936c34b2c4fe3d8db34d58dd
Izhaan Raza <66744194+Izhaan-Raza@users.noreply.github.com>
2024-10-27 03:57:05 +0530

    4 am

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
       
       {currentPage === 'ai' && (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <h1 className="text-3xl font-bold text-gray-900">AI Help</h1>
+          
           {
             <iframe src='https://izhaan-raza.github.io/chatBot/' width="100%" height="600px"></iframe>
           }
